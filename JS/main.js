28da4cb492f527f91a3de26cc1ef65f00972d5f1
Danil Dihavtsov <danildigavcov@gmail.com>
2026-02-23 12:51:12 +0200

    Added Delete button, and priority. Update JS, HTML, CSS

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,54 @@ const priority = document.getElementById("select-priority")
 const taskList = document.getElementById("task-list")
 const h3 = document.getElementById("h3")
 
+
+function formcreate (h3Value, inputValue, priorityValue) {
+
+const TaskElement = document.createElement("div");
+TaskElement.classList.add("task");
+const TaskInfo = document.createElement("div");
+TaskInfo.classList.add("task-info")
+
+const TaskH3 = document.createElement("h3");
+const TaskText = document.createElement("div");
+const TaskDate = document.createElement("div");
+
+TaskInfo.append(TaskH3, TaskText, TaskDate)
+
+const TaskPriorityList = document.createElement("div");
+TaskPriorityList.classList.add("priority-list");
+
+const TaskPriority = document.createElement("div");
+const TaskButton = document.createElement("button");
+TaskButton.textContent = "Delete";
+
+TaskButton.addEventListener("click",function() {
+    TaskElement.remove()
+})
+
+TaskPriorityList.append(TaskPriority,TaskButton);
+
+TaskH3.textContent = h3Value;
+TaskText.textContent = inputValue;
+TaskPriority.textContent = priorityValue;
+
+TaskElement.append(TaskInfo, TaskPriorityList);
+
+taskList.append(TaskElement);
+}
+
+function checkform (inputValue, h3Value, priorityValue){
+    if(inputValue === "" ){
+    alert ("Input your Task")
+    return false
+}
+else if (h3Value === ""){
+    alert ("Input your Task Name")
+    return false
+}
+    return true;
+}
+
 form.addEventListener("submit", function(event) {
 event.preventDefault();
 
@@ -11,25 +59,20 @@ const inputValue = input.value;
 const priorityValue = priority.value;
 const h3Value = h3.value;
 
-if(inputValue === "" )
-    alert ("Input your Task")
-else if (h3Value === "")
-    alert ("Input your Task Name")
-else {
-
-const TaskElement = document.createElement("div");
-TaskElement.classList.add("task");
-
-const TaskH3 = document.createElement("h3");
-const TaskText = document.createElement("div");
-
-TaskElement.append(TaskH3, TaskText);
-
-TaskH3.textContent = h3Value;
-TaskText.textContent = inputValue;
-
-taskList.append(TaskElement);
+if(checkform(inputValue, h3Value)){
+formcreate(h3Value, inputValue, priorityValue);
+};
 
 input.value = "";
 h3.value = "";
-}})
+})
+
+
+
+if(priorityValue === "Low"){
+    TaskPriority.classList.add("Low");
+} else if (TaskPriority === "Medium"){
+    TaskPriority.classList.add("Medium");
+} else if (TaskPriority === "High"){
+    TaskPriority.classList.add("High");
+}
