a69f4e8c38920203fb9d67e0df75c1ab81733c39
Danil Dihavtsov <danildigavcov@gmail.com>
2026-02-24 20:51:28 +0200

    Add date

--- JS/main.js
+++ JS/main.js
@@ -8,39 +8,38 @@ const h3 = document.getElementById("h3")
 function formcreate (h3Value, inputValue, priorityValue) {
 
 const TaskElement = document.createElement("div");
-TaskElement.classList.add("task");
 const TaskInfo = document.createElement("div");
-TaskInfo.classList.add("task-info")
-
 const TaskH3 = document.createElement("h3");
 const TaskText = document.createElement("div");
 const TaskDate = document.createElement("div");
-
-TaskInfo.append(TaskH3, TaskText, TaskDate)
-
+const now = new Date().toLocaleString("uk-UA");
 const TaskPriorityList = document.createElement("div");
-TaskPriorityList.classList.add("priority-list");
-
 const TaskPriority = document.createElement("div");
 const TaskButton = document.createElement("button");
-TaskButton.textContent = "Delete";
+
+TaskElement.classList.add("task");
+TaskInfo.classList.add("task-info")
+TaskDate.classList.add("date")
+TaskPriorityList.classList.add("priority-list");
+
+TaskInfo.append(TaskH3, TaskText, TaskDate);
+TaskElement.append(TaskInfo, TaskPriorityList);
+taskList.append(TaskElement);
+TaskPriorityList.append(TaskPriority,TaskButton);
 
 TaskButton.addEventListener("click",function() {
     TaskElement.remove()
 })
 
-TaskPriorityList.append(TaskPriority,TaskButton);
-
+TaskButton.textContent = "Delete";
 TaskH3.textContent = h3Value;
 TaskText.textContent = inputValue;
 TaskPriority.textContent = priorityValue;
+TaskDate.textContent = now;
 
-TaskElement.append(TaskInfo, TaskPriorityList);
-
-taskList.append(TaskElement);
 }
 
-function checkform (inputValue, h3Value, priorityValue){
+function checkform (inputValue, h3Value){
     if(inputValue === "" ){
     alert ("Input your Task")
     return false
@@ -61,18 +60,4 @@ const h3Value = h3.value;
 
 if(checkform(inputValue, h3Value)){
 formcreate(h3Value, inputValue, priorityValue);
-};
-
-input.value = "";
-h3.value = "";
-})
-
-
-
-if(priorityValue === "Low"){
-    TaskPriority.classList.add("Low");
-} else if (TaskPriority === "Medium"){
-    TaskPriority.classList.add("Medium");
-} else if (TaskPriority === "High"){
-    TaskPriority.classList.add("High");
-}
+}});
